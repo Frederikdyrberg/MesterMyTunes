e04f512abcd4902499d391c435178e84bd30c999
Patricl <mussi_kat@hotmail.com>
2016-12-06 12:05:53 +0100

    close

--- MyTunes/src/mytunes/GUI/controller/PlaylistController.java
+++ MyTunes/src/mytunes/GUI/controller/PlaylistController.java
@@ -7,10 +7,12 @@ package mytunes.GUI.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import mytunes.be.MyTunes;
 
 /**
@@ -40,5 +42,14 @@ public class PlaylistController implements Initializable {
     void setMytunes(MyTunes newPlaylist) {
      currentPlaylist = newPlaylist;
     }
+
+    @FXML
+    private void playlistCancel(ActionEvent event) {
+     // get a handle to the stage
+    Stage stage = (Stage) playlistCancel.getScene().getWindow();
+    // do what you have to do
+    stage.close();
+}
+    
     
 }
