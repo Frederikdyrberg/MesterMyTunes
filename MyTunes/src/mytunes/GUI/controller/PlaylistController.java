449e02381d42b064a2a6f00a3943ab2eb6eeed62
Frederikdyrberg <Frederikdyrberg@users.noreply.github.com>
2016-12-06 11:27:56 +0100

    Add files via upload

---
+++ MyTunes/src/mytunes/GUI/controller/PlaylistController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.GUI.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import mytunes.be.MyTunes;
+
+/**
+ * FXML Controller class
+ *
+ * @author FrederikD
+ */
+public class PlaylistController implements Initializable {
+
+    @FXML
+    private TextField playlistName;
+    @FXML
+    private Button playlistCancel;
+    @FXML
+    private Button playlistSave;
+    
+    private MyTunes currentPlaylist;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    void setMytunes(MyTunes newPlaylist) {
+     currentPlaylist = newPlaylist;
+    }
+    
+}
